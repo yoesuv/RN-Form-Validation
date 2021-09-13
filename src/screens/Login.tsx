b2372d0e27432526ff3583c7012703661af400cd
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-09-13 10:33:02 +0700

    :art: form login UI

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,14 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, KeyboardAvoidingView, TextInput } from 'react-native';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import SizedBox from '../components/SizedBox';
+import { THEME_COLOR } from '../data/Colors';
 
 export default function Login({ navigation }) {
+
+  interface ILoginInput {
+    email: String;
+    password: String;
+  }
+
+  const { register, handleSubmit } = useForm<ILoginInput>();
+  const onSubmit: SubmitHandler<ILoginInput> = data => console.log(data);
+
   return (
     <View style={styles.container}>
-      <Text>Login Screen</Text>
-      <Button title='Register' onPress={() => {
-        console.log('go to Register');
-        navigation.navigate('Register');
-      }}/>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <Text style={styles.label}>Email</Text>
+        <SizedBox height={5} />
+        <TextInput
+          autoCapitalize="none"
+          autoCompleteType="email"
+          autoCorrect={false}
+          keyboardType="email-address"
+          returnKeyType="next"
+          textContentType="username"
+          style={styles.input}
+        />
+
+        <SizedBox height={10} />
+        <Text style={styles.label}>Password</Text>
+        <SizedBox height={5} />
+        <TextInput
+          autoCapitalize="none"
+          autoCompleteType="password"
+          autoCorrect={false}
+          returnKeyType="done"
+          secureTextEntry
+          textContentType="password"
+          style={styles.input}
+        />
+
+        <SizedBox height={20} />
+        <Button color='#009688' title='Login' onPress={() => {
+          console.log('Login');
+        }}/>
+        <SizedBox height={20} />
+        <Button color='#009688' title='Register' onPress={() => {
+          console.log('go to Register');
+          navigation.navigate('Register');
+        }}/>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -17,9 +60,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 16
   },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  input: {
+    borderColor:'#EEEEEE',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderRadius: 8,
+    height: 40,
+    paddingVertical: 10,
+    paddingHorizontal: 10
+  },
+  button: {
+    color: THEME_COLOR
+  }
 });
 
 export default Login;
