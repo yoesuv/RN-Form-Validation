d2cf04a3e7a38b8dac332176fe53991a394b9a16
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-09-13 16:28:54 +0700

    :art: button disabled style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { Text, View, StyleSheet, Pressable } from 'react-native';
 
 export default function Button(props) {
-  const { onPress, title = 'Save' } = props;
-  return (
-    <Pressable style={styles.button} onPress={onPress}>
-      <Text style={styles.text}>{title}</Text>
-    </Pressable>
-  );
+  const { onPress, title = 'Save', disabled = false} = props;
+  if (disabled) {
+    return (
+      <Pressable disable={true} style={styles.buttonDisabled}>
+        <Text style={styles.text}>{title}</Text>
+      </Pressable>
+    );
+  } else {
+    return (
+      <Pressable disable={false} style={styles.button} onPress={onPress}>
+        <Text style={styles.text}>{title}</Text>
+      </Pressable>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -18,6 +27,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     borderRadius: 8,
     backgroundColor: '#009688',
+  },
+  buttonDisabled: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 8,
+    backgroundColor: '#9E9E9E',
   },
   text: {
     fontSize: 16,
