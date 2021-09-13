e830473b25e9640888421f4dba71ebc5eb945d61
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-09-13 08:47:17 +0700

    :art: setup toolbar

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Splash from './src/screens/Splash';
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
+import { THEME_COLOR, HEADER_TINT_COLOR } from './src/data/Colors';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +13,26 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
-        <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
-        <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
+        <Stack.Screen name="Login" component={Login} options={{
+          title: 'Login',
+          headerStyle: {
+            backgroundColor: THEME_COLOR
+          },
+          headerTintColor: HEADER_TINT_COLOR,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          }
+        }} />
+        <Stack.Screen name="Register" component={Register} options={{
+          title: 'Register',
+          headerStyle: {
+            backgroundColor: THEME_COLOR
+          },
+          headerTintColor: HEADER_TINT_COLOR,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          }
+        }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/data/Colors.tsx
@@ -0,0 +1,2 @@
+export const THEME_COLOR = "#009688";
+export const HEADER_TINT_COLOR = "#FFFFFF"
