c128983ed94690b9ac9359e25b6c50147ef1d6d7
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-21 09:09:51 +0700

    :newspaper: create navigations

--- App.tsx
+++ App.tsx
@@ -1,39 +1,8 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Splash from './src/screens/Splash';
-import Login from './src/screens/Login';
-import Register from './src/screens/Register';
-import { THEME_COLOR, HEADER_TINT_COLOR } from './src/data/Colors';
-
-const Stack = createNativeStackNavigator();
+import AppNavigation from './src/Navigations';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
-        <Stack.Screen name="Login" component={Login} options={{
-          title: 'Login',
-          headerStyle: {
-            backgroundColor: THEME_COLOR
-          },
-          headerTintColor: HEADER_TINT_COLOR,
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          }
-        }} />
-        <Stack.Screen name="Register" component={Register} options={{
-          title: 'Register',
-          headerStyle: {
-            backgroundColor: THEME_COLOR
-          },
-          headerTintColor: HEADER_TINT_COLOR,
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          }
-        }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AppNavigation />
   );
 }
---
+++ src/Navigations.tsx
@@ -0,0 +1,31 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackNavigationOptions } from '@react-navigation/native-stack';
+
+import Splash from './screens/Splash';
+import Login from './/screens/Login';
+import Register from './/screens/Register';
+import { THEME_COLOR, HEADER_TINT_COLOR } from './data/Colors';
+
+const Stack = createNativeStackNavigator();
+const baseOptions: NativeStackNavigationOptions = {
+    title: 'Login',
+    headerStyle: {
+    backgroundColor: THEME_COLOR
+    },
+    headerTintColor: HEADER_TINT_COLOR,
+    headerTitleStyle: {
+    fontWeight: 'bold',
+    },
+}
+
+const AppNavigation = () => {
+    return <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
+        <Stack.Screen name="Login" component={Login} options={{...baseOptions, title: 'Login'}} />
+        <Stack.Screen name="Register" component={Register} options={{...baseOptions, title: 'Register'}} />
+        </Stack.Navigator>
+    </NavigationContainer>
+}
+
+export default AppNavigation;
